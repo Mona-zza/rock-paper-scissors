7673ea1a81592e59093b8a77d36e5428fcbf9a1e
Mona <monazza725@gmail.com>
2026-08-22 16:30:12 +0530

    Add files via upload

---
+++ rock paper scissor/script.js
@@ -0,0 +1,35 @@
+const playerdisplay = document.getElementById("player-choice");
+const computerdisplay = document.getElementById("computer-choice");
+const resultdisplay = document.getElementById("resultdisplay");
+
+const choices = ["rock", "paper", "scissors"];
+
+function game(playerchoice) {
+
+    const computerchoice = choices[Math.floor(Math.random() * 3)];
+
+    const getresult = (playerchoice, computerchoice) =>
+        playerchoice === computerchoice
+            ? "IT IS A TIE! 😕"
+            : (playerchoice === "rock" && computerchoice === "scissors") ||
+              (playerchoice === "paper" && computerchoice === "rock") ||
+              (playerchoice === "scissors" && computerchoice === "paper")
+            ? "YOU WIN! 😍"
+            : "YOU LOSE 😔";
+
+    const result = getresult(playerchoice, computerchoice);
+
+    resultdisplay.textContent = result;
+    playerdisplay.textContent = `PLAYER: ${playerchoice}`;
+    computerdisplay.textContent = `COMPUTER: ${computerchoice}`;
+
+    resultdisplay.style.backgroundColor =
+        result === "IT IS A TIE! 😕"
+            ? "blue"
+            : result === "YOU WIN! 😍"
+            ? "green"
+            : "red";
+
+    resultdisplay.style.border = "2px solid transparent";
+    resultdisplay.style.color = "white";
+}
